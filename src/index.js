8626490ba9d70bab22505446d3807adbaf039361
Antonina1909 <antonina1909@yandex.ru>
2023-01-19 00:23:13 +0500

    init project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.html';
+import './index.scss';
+import { renderFooter } from './modules/render/renderFooter';
+import { renderHeader } from './modules/render/renderheader';
+
+
+renderHeader();
+renderFooter();
+
+mainPage()
---
+++ src/modules/mainPage/mainPage.js
@@ -0,0 +1,4 @@
+
+renderNavigation()
+renderHero()
+renderProducts()
